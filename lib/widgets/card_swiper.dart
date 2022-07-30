5901e2dbc8a2a14f794a251f4c6033bd3cae0fd5
Nahuel Vidales <83135275+NahuVidales@users.noreply.github.com>
2022-07-29 23:56:36 -0300

    swiper changes

--- lib/widgets/card_swiper.dart
+++ lib/widgets/card_swiper.dart
@@ -1,4 +1,4 @@
-
+import 'package:card_swiper/card_swiper.dart';
 import 'package:flutter/material.dart';
 
 class CardSwiper extends StatelessWidget {
@@ -6,10 +6,22 @@ class CardSwiper extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+
     return Container(
-      height: 500,
+      height: size.height * 0.9,
       width: double.infinity,
       color: Colors.red,
+      child: Swiper(itemCount: 10,
+      itemWidth: size.width * 40,
+      layout: SwiperLayout.STACK,
+      itemHeight: size.height * 50,
+      itemBuilder: (BuildContext context, int index) {
+        return FadeInImage(
+          placeholder: AssetImage('assets\loading.gif'),
+          image: NetworkImage('https://images.unsplash.com/photo-1617703376300-58b4220ffc0b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=327&q=80'));
+      },
+      ),
     );
   }
 }

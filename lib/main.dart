abc9b223ff822c383ff07cebcc9eddc669b585e0
Nahuel Vidales <83135275+NahuVidales@users.noreply.github.com>
2022-10-14 00:59:51 -0300

    some changes in HTML request

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:movie_app/screens/screens.dart';
 
-void main() => runApp(const MyApp());
+void main() => runApp(AppState());
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
 
+class AppState extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: ( _ ) => MoviesProvider(), lazy: false ),
+      ],
+      child: MyApp(),
+    );
+  }
+}
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
